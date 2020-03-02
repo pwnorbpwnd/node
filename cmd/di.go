6439e12e15773dae90eca04b23bfe4aadddd48d6
anjmao <andzej.maciusovic@gmail.com>
2020-03-02 13:24:32 +0200

    Validate consumer balance during connect

--- cmd/di.go
+++ cmd/di.go
@@ -527,6 +527,7 @@ func (di *Dependencies) bootstrapNodeComponents(nodeOptions node.Options, tequil
 		di.IPResolver,
 		connection.DefaultIPCheckParams(),
 		connection.DefaultStatsReportInterval,
+		di.ConsumerBalanceTracker.GetBalance,
 	)
 
 	di.LogCollector = logconfig.NewCollector(&logconfig.CurrentLogOptions)
